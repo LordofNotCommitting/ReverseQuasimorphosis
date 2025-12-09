cea693cbc796ddf47e81c820e751862734b6c267
LordofNotCommitting <shadymcshade247@gmail.com>
2025-12-09 00:44:22 -0500

    1.1

--- src/SetQuasiMinPerAP.cs
+++ src/SetQuasiMinPerAP.cs
@@ -27,21 +27,78 @@ namespace ReverseQuasimorphosis
         static int Passive_Effect_QGain_Cap_Max = Plugin.ConfigGeneral.ModData.GetConfigValue<int>("Passive_Effect_QGain_Cap_Max", 1);
 
 
-        public static bool Prefix(QmorphosController __instance)
-        {
-            // --- SAFE CALL TO base.ProcessActionPoint() ---
-            // Equivalent to: base.ProcessActionPoint();
-            MethodInfo baseMethod = AccessTools.Method(typeof(Turnable), "ProcessActionPoint");
-            baseMethod.Invoke(__instance, null);
 
-            //squash max quasimorphosis if it is on
+        public static bool Prefix(ref QmorphosController __instance)
+        {
             if (Quasi_MaxReduction_IsOn && !__instance._raidMetadata.QMorphosBaronSpawned && __instance._raidMetadata.QMorphosMinLevel == __instance._raidMetadata.QMorphosLevel && __instance._raidMetadata.QMorphosMinLevel > 0 && __instance._raidMetadata.QMorphosMinLevel < 1000)
             {
                 __instance._raidMetadata.QMorphosMinLevel = Mathf.Max(__instance._raidMetadata.QMorphosMinLevel - Quasi_MaxReduction_Amount, 0);
                 __instance.QmorphosValue = Mathf.Min(__instance._raidMetadata.QMorphosMinLevel + Quasi_MaxReduction_Cost, 1000);
                 __instance.InvokeRefreshStage(__instance.Now, true);
             }
+            saved_qval = __instance.QmorphosValue;
+            //Plugin.Logger.Log("saved quasi" + saved_qval);
+            return true;
+        }
+        public static void Postfix(QmorphosController __instance)
+        {
 
+            int default_multi = (Passive_Default_QGain_Reverted ? -1 : 1);
+            int weightedIndex = DropManager.GetWeightedIndex(Data.Global.QmorphosPerTurnWeights);
+            int num = Mathf.Clamp(Mathf.RoundToInt(__instance._creatures.Player.CreatureData.EffectsController.SumEffectsValue<WoundEffectQmorph>((WoundEffectQmorph w) => (float)w.QmorphValue)), Passive_Effect_QGain_Cap_Min, Passive_Effect_QGain_Cap_Max);
+            int num2 = Mathf.RoundToInt((float)((Data.Global.QmorphosPerTurnPoints[weightedIndex] * default_multi) + num + Passive_AdditDefault_QGain) * __instance._difficulty.Preset.QmorphLevelGrowth);
+
+
+            //Plugin.Logger.Log("loaded quasi" + saved_qval);
+            //loaded save or such? so this value could be uninitialized.
+            if (saved_qval == 0)
+            {
+                saved_qval = __instance.QmorphosValue;
+            }
+
+            QmorphosRecord recordByLevel = __instance.GetRecordByLevel(saved_qval, __instance._raidMetadata.BramfaturaId);
+            saved_qval += num2;
+            QmorphosRecord recordByLevel2 = __instance.GetRecordByLevel(saved_qval, __instance._raidMetadata.BramfaturaId);
+
+            //__instance.QmorphosValue = 0;
+            //need to re-proc... quasi behaviors, since setting it to value did not exactly  (implication unknown)
+            __instance.QmorphosValue = saved_qval;
+
+            
+            // no idea what the default behavior is for invasion. but it is not global aggro I think
+            if (__instance._missions.Get(__instance._raidMetadata).ProcMissionType != ProceduralMissionType.BramfaturaInvasion) {
+                //seeing if this fix derpy quasis
+                //make them behave as usual upon the correct conditions.
+                if (__instance.QmorphosValue >= 800)
+                {
+                    __instance.SetGlobalEndlessHunt(true);
+                }
+                else
+                {
+                    __instance.SetGlobalEndlessHunt(false);
+                }
+            }
+            
+
+        }
+
+        /*
+        public static bool Prefix(QmorphosController __instance)
+        {
+            // something wrong with turnable call when defending against quasis.
+            // so this function is not usable.
+            MethodInfo baseMethod = AccessTools.Method(typeof(Turnable), "ProcessActionPoint");
+            baseMethod.Invoke(__instance, null);
+            
+            //squash max quasimorphosis if it is on
+            
+            if (Quasi_MaxReduction_IsOn && !__instance._raidMetadata.QMorphosBaronSpawned && __instance._raidMetadata.QMorphosMinLevel == __instance._raidMetadata.QMorphosLevel && __instance._raidMetadata.QMorphosMinLevel > 0 && __instance._raidMetadata.QMorphosMinLevel < 1000)
+            {
+                __instance._raidMetadata.QMorphosMinLevel = Mathf.Max(__instance._raidMetadata.QMorphosMinLevel - Quasi_MaxReduction_Amount, 0);
+                __instance.QmorphosValue = Mathf.Min(__instance._raidMetadata.QMorphosMinLevel + Quasi_MaxReduction_Cost, 1000);
+                __instance.InvokeRefreshStage(__instance.Now, true);
+            }
+            
             // --- ORIGINAL METHOD BODY EXACT COPY ---
 
             if (__instance._raidMetadata.StationVisit)
@@ -66,49 +123,7 @@ namespace ReverseQuasimorphosis
 
             return false;
         }
-
-
-
-        /*
-        public static bool Prefix(ref QmorphosController __instance)
-        {
-            if (Quasi_MaxReduction_IsOn && !__instance._raidMetadata.QMorphosBaronSpawned && __instance._raidMetadata.QMorphosMinLevel == __instance._raidMetadata.QMorphosLevel && __instance._raidMetadata.QMorphosMinLevel > 0 && __instance._raidMetadata.QMorphosMinLevel < 1000)
-            {
-                __instance._raidMetadata.QMorphosMinLevel = Mathf.Max(__instance._raidMetadata.QMorphosMinLevel - Quasi_MaxReduction_Amount, 0);
-                __instance.QmorphosValue = Mathf.Min(__instance._raidMetadata.QMorphosMinLevel + Quasi_MaxReduction_Cost, 1000);
-                __instance.InvokeRefreshStage(__instance.Now, true);
-            }
-            saved_qval = __instance.QmorphosValue;
-            //Plugin.Logger.Log("saved quasi" + saved_qval);
-            return true;
-        }
-        public static void Postfix(QmorphosController __instance)
-        {
-
-            int default_multi = (Passive_Default_QGain_Reverted ? -1 : 1);
-            int weightedIndex = DropManager.GetWeightedIndex(Data.Global.QmorphosPerTurnWeights);
-            int num = Mathf.Clamp(Mathf.RoundToInt(__instance._creatures.Player.CreatureData.EffectsController.SumEffectsValue<WoundEffectQmorph>((WoundEffectQmorph w) => (float)w.QmorphValue)), Passive_Effect_QGain_Cap_Min, Passive_Effect_QGain_Cap_Max);
-            int num2 = Mathf.RoundToInt((float)((Data.Global.QmorphosPerTurnPoints[weightedIndex] * default_multi) + num + Passive_AdditDefault_QGain) * __instance._difficulty.Preset.QmorphLevelGrowth);
-
-
-            //Plugin.Logger.Log("loaded quasi" + saved_qval);
-            //loaded save or such? so this value could be uninitialized.
-            if (saved_qval == 0) {
-                saved_qval = __instance.QmorphosValue;
-            }
-
-            QmorphosRecord recordByLevel = __instance.GetRecordByLevel(saved_qval, __instance._raidMetadata.BramfaturaId);
-            saved_qval += num2;
-            QmorphosRecord recordByLevel2 = __instance.GetRecordByLevel(saved_qval, __instance._raidMetadata.BramfaturaId);
-
-            __instance.QmorphosValue = saved_qval;
-
-            if (recordByLevel != recordByLevel2)
-            {
-                __instance.InvokeRefreshStage(recordByLevel2, true);
-            }
-
-        }
         */
+
     }
 }
